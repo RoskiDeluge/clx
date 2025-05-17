939c50bf2bc56af47c12468b57ab27887c81acff
Skyler Kimura <skyler8@hawaii.edu>
2025-05-16 15:48:23 -1000

    updated dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         "requests",
-        "pyperclip"
+        "pyperclip",
+        "openai",
+        "python-dotenv"
     ],
     entry_points={
         "console_scripts": [
